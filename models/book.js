4181c0cfd0eadbc1a9f8d3559965551b4d468a36
bhupeshbc <bhupeshbc12@gmail.com>
2023-04-27 11:09:45 +0545

    I don't know anything

--- models/book.js
+++ models/book.js
@@ -1,4 +1,11 @@
 const mongoose= require('mongoose')
+const reviewSchema = new mongoose.Schema({
+    text :{
+        type : String,
+        required : true, 
+        minLength : 10
+    }
+})
 
 const  bookSchema = new mongoose.Schema({
     title:{
@@ -8,7 +15,10 @@ const  bookSchema = new mongoose.Schema({
     author: {
         type: String,
         default: 'Anonymous'
-    }
+    }, 
+    reviews:[reviewSchema]
+
+
 }, {timestamps: true})
 
 module.exports = mongoose.model('Book', bookSchema)
