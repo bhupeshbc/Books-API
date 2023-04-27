32d76292b5f58a4a3c2aff2045b7387418442ff6
bhupeshbc <bhupeshbc12@gmail.com>
2023-04-27 11:09:23 +0545

    I don't know anything

--- routes/book-routes.js
+++ routes/book-routes.js
@@ -1,33 +1,35 @@
+
 const express = require('express');
 const books = require('../data/books');
-const Book = require('../models/book');
-const book = require('../models/book');
+const Book = require('../models/Book');
+const book = require('../models/Book');
 
 const router = express.Router()
 
-// This makes codes organized rather then writing everthing in one file
+// This makes codes organized rather then writing everything in one file
 router.route('/')
-    .get(async (req, res) => {
-        // Approch 1 
-        // (Better)(async not needed in function)
+    .get(async (req, res, next) => {
+        // Approach 1 (Better)(async not needed in function)
         // Book.find()
         //     .then (books => res.json(books))
         //     .catch (err => console.log(err))
 
-        // Approch 2 
+        // Approach 2 
         try {
             const books = await Book.find() 
             res.json(books)
         } catch (error) {
-            console.log(error);
+            next;
         }   
         
     })
-    .get((req, res) => {
+    .get((req, res, next) => {
         res.json(books)
         Book.find(req.body) 
             .then((book) => res.json(books))
-            .catch(err => console.log(err))
+            .catch(next)
+            // .catch(err => next(err))
+            // Error handling middleware
         // try {
         //     const books = await Book.find()
         //     res.json(books)
@@ -52,36 +54,46 @@ router.route('/')
 
         Book.create(req.body)
         .then((book) => res.status(201).json(book))
-        .catch(err => next(err))
+        // .catch(err => console.log(err))
+        .catch(next)
     })
 
     .put((req, res) => {
         res.status(405).json({ error:"This method (PUT) is not allowed" })
     })
 
-    .delete((req, res) => {
+    .delete((req, res, next) => {
         book.deleteMany()
             .then(reply => res.json(reply))
-            .catch(err => next(err))
+            // .catch(err => console.log(err))
+            .catch(next)
     })
     
 router.route('/:book_id')
-    .get((req, res) => {
+    .get((req, res, next) => {
         // const book = books.find((b) => b.id === parseInt(req.params.book_id));
         // if (!book) {
         //     return res.status(404).json({ error: 'Book not found' });
         // }
         // res.json(book);
         Book.findById(req.params.book_id)
-        .then((book) => res.json(book))
-        .catch(err => next(err))
+        // .then((book) => res.json(book))
+        .then((book) => {
+                if(!book) {
+                    res.status(404).json({ error: "Book not found" }) 
+                }
+                res.json(book)
+            })
+        // .catch(err => console.log(err))
+        .catch(next)
+
     })
 
     .post((req, res) => {
         res.status(405).json({error: 'This method (POST) is not allowed'})
     })
 
-    .put((req, res) => {
+    .put((req, res, next) => {
         // const updated_books = books.map((b) => {
         // if(b.id == req.params.book_id){
         //     b.title = req.body.title
@@ -96,10 +108,11 @@ router.route('/:book_id')
             { new : true}
         )
         .then(updated => res.json(updated))
-        .catch(err => next(err))
+        // .catch(err => console.log(err))
+        .catch(next)
     })
 
-    .delete((req, res) => {
+    .delete((req, res, next) => {
         // const index = books.findIndex((b) => b.id === parseInt(req.params.book_id));
         // if (index === -1) {
         //   return res.status(404).json({ error: 'Book not found' });
@@ -108,16 +121,29 @@ router.route('/:book_id')
         // res.json(books);
         Book.findOneAndDelete(req.params.book_id)
             .then(reply => res.status(204).end())
-            .catch(err => next(err))
-    });
-      
-    // .delete((req, res) => {
-    //     books = books.filter((b) => {
-    //         return b.id != req.params.book_id
-    //     })
-    //     res.json(books)
-    // });
-
-      
+            // .catch(err => console.log(err))
+            .catch(next)
+        });
+      router.route('./book_id/reviews')
+      .get((req, res, next)=>{
+        Book.findById(req.params.book_id)
+            .then((book)=>{
+                if(!book) return res.status(404).json({error : 'book not found'})
+                res.json(book.reviews)
+            }).catch(next)
+      })
+      .post((req, res, next)=>{
+        Book.findById(req.params.book_id)
+        .then((book)=>{
+            if(!book) return res.status(404).json({error: 'book not found'})
+            const review = {
+                text :req.body.text
+            }
+            book.reviews.push(review)
+            book.save()
+            .then((book)=> res.status(201).json(book.review))
+            .catch(next)
+        }).catch(next)
+      })
 
 module.exports = router
